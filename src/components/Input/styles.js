bcd0a9e2f0a9ba7c0048fe211cda6f868f8d00e7
Diego Nunes <diegodnunes12@gmail.com>
2023-12-05 18:21:47 -0300

    criando o component de input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const InputContainer = styled.div`
+    width: 80%;
+    height: 62px;
+    margin-top: 20px;
+
+    input {
+        width: 100%;
+        height: 100%;
+        color: #E6EDF3;
+        font-size: 18px;
+        
+        padding: 5px 20px;
+        background: transparent;
+        border: solid 1px #E6EDF3;
+        border-radius: 20px;
+    }
+`;
+
+export default InputContainer;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './styles';
+import { Container } from './styles';
+import Input from '../components/Input';
+import logo from '../assets/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      teste
-    </div>
+    <Container>
+      <img src={logo} width={72} alt="logo github" />
+      <Input />
+    </Container>
   );
 }
 
